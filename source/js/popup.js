8318e3b31a9fa23abba4a3f98ea689358fae684b
anactasi2607 <anactasi2607@yandex.ru>
2019-05-27 20:40:52 +0300

    Сделала проверку корректности заполнения формы подбора

--- source/js/popup.js
+++ source/js/popup.js
@@ -2,6 +2,14 @@ var link = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".site-list");
 var close = document.querySelector(".main-nav");
 
+var form = document.querySelector(".filter");
+var petName = document.querySelector(".questionnaire__name");
+var petWeight = document.querySelector(".questionnaire__weight");
+var ownerMail = document.querySelector(".owner__mail");
+var ownerTel = document.querySelector(".owner__tel");
+
+
+/*Для открытия-закрытия меню на мобильной версии*/
 link.classList.remove("main-nav__toggle--nojs");
 link.classList.add("main-nav__toggle--closed");
 menu.classList.remove("site-list--nojs");
@@ -22,3 +30,34 @@ link.addEventListener("click", function (evt) {
   }
 });
 
+/*Для валидации формы подбора программы*/
+
+form.addEventListener("submit", function (evt) {
+  if (!ownerTel.value) {
+    evt.preventDefault();
+    ownerTel.classList.remove("error");
+    ownerTel.classList.add("error");
+    ownerTel.focus();
+  }
+
+  if (!ownerMail.value) {
+    evt.preventDefault();
+    ownerMail.classList.remove("error");
+    ownerMail.classList.add("error");
+    ownerMail.focus();
+  }
+
+  if (!petWeight.value) {
+    evt.preventDefault();
+    petWeight.classList.remove("error");
+    petWeight.classList.add("error");
+    petWeight.focus();
+  }
+
+  if (!petName.value) {
+    evt.preventDefault();
+    petName.classList.remove("error");
+    petName.classList.add("error");
+    petName.focus();
+  }
+});
